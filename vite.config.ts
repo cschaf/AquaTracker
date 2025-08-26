984ad8089bb4651ff2dc35647624f45b202bb941
Christian Schaf <cschaf@outlook.com>
2025-08-26 14:15:19 +0200

    updated files for deployment with github actions

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   base: "/AquaTracker/",
+  build: {
+    outDir: 'dist'
+  },
   server: {
     open: true,
     port: 3001,
