9fb9d2a62a66a0f4813709565aa3d25be603b14c
Christian Schaf <cschaf@outlook.com>
2025-08-28 13:31:51 +0200

    Added vite-plugin-pwa config

--- vite.config.ts
+++ vite.config.ts
@@ -3,14 +3,56 @@ import type { UserConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import type { InlineConfig } from 'vitest'
+import { VitePWA } from 'vite-plugin-pwa'
 
 interface VitestConfigExport extends UserConfig {
   test: InlineConfig
 }
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(), VitePWA({
+    registerType: 'autoUpdate',
+    workbox: {
+      globPatterns: ['**/*.{js,css,html,ico,png,svg,woff2}'],
+      runtimeCaching: [
+        {
+          urlPattern: /^https:\/\/aquatracker\.schaflabs\.com\/.*/i,
+          handler: 'CacheFirst',
+          options: {
+            cacheName: 'api-cache',
+          }
+        }
+      ]
+    },
+    includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
+    manifest: {
+      name: 'AquaTracker - Daily Water Intake Monitor',
+      short_name: 'AquaTracker',
+      description: 'Monitor and log your daily water intake. Stay hydrated with AquaTracker - make tracking your hydration goals easy and enjoyable.',
+      theme_color: '#2196F3',
+      background_color: '#FFFFFF',
+      display: 'standalone',
+      scope: '/',
+      start_url: '/',
+      icons: [
+        {
+          src: 'icons/icon-192x192.png',
+          sizes: '192x192',
+          type: 'image/png'
+        },
+        {
+          src: 'icons/icon-512x512.png',
+          sizes: '512x512',
+          type: 'image/png'
+        }
+      ]
+    },
+    devOptions: {
+      enabled: true // PWA in development mode aktivieren
+    }
+  })],
   base: "/",
+
   build: {
     outDir: 'dist'
   },
