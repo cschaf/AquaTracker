cc8cbe01109f3e7a41f2ff4050668be76270c6f2
Christian Schaf <cschaf@outlook.com>
2025-09-12 09:39:44 +0200

    Update color for error toast message

--- src/presentation/services/toast.service.ts
+++ src/presentation/services/toast.service.ts
@@ -18,12 +18,12 @@ const toastOptions = {
   },
   error: {
     style: {
-      background: 'var(--color-warning)',
+      background: 'var(--color-destructive)',
       color: 'var(--color-text-on-primary)',
     },
     iconTheme: {
       primary: 'var(--color-text-on-primary)',
-      secondary: 'var(--color-warning)',
+      secondary: 'var(--color-destructive)',
     },
   },
 };
